9f55e7e7c9d206691cfa071e376241c34d2f8bd0
liuyuaustin@gmail.com <austin@prunus-mume.iams.sinica.edu.tw>
2016-01-13 16:54:59 -0800

    refactored loop()

--- src/shell/mysh.c
+++ src/shell/mysh.c
@@ -25,6 +25,22 @@ int main() {
     return 0;
 }
 
+
+void print_prompt(){
+    char *prompt = (char *) malloc(sizeof(char) * MAX_BUFSIZE);
+    strcat(prompt, "\0");
+    char *username;
+    char *dir_curr;
+
+    username = getlogin();      // Username of the session
+    // Current directory
+    dir_curr = getcwd(NULL, MAX_BUFSIZE); // Need to free this?
+    // Getting the current prompt
+    strcat(strcat(strcat(strcat(prompt, username), ":"), dir_curr), ">");
+    printf("%s ", prompt);
+}
+
+
 #define MAX_TOK_BUFSIZE 64
 char **tokenize(command *cmnd_struct){
     char **tokens = malloc(MAX_TOK_BUFSIZE*sizeof(char*)); // array of tokens
@@ -48,59 +64,34 @@ char **tokenize(command *cmnd_struct){
     return tokens;
 }
 
-int loop() {
-    char *username;
-    char *dir_curr;
-    char prompt[500] = "\0";
-    int exitcode;
+
+void exec_cmd(command *cmd){
+    int debug = 1;
     char *dir_home = (char *) malloc(sizeof(char) * MAX_BUFSIZE);
-
-    parsed *line = (parsed *) malloc(sizeof(parsed));
-    line->frst = NULL;
-    line->curr = NULL;
-    line->error = 0;
-    // TODO: null check
-
-    /* root = NULL; */
-    /* cmd->first_token = root; */
-
-    exitcode = 0;
-
-    // Username of the session
-    username = getlogin();
-    // Home directory of the user
-    dir_home = getenv("HOME");
-    // Current directory
-    dir_curr = getcwd(NULL, MAX_BUFSIZE); // Need to free this?
-    // Getting the current prompt
-    strcat(strcat(strcat(strcat(prompt, username), ":"), dir_curr), ">");
-    // Displaying the prompt
-    printf("%s ", prompt);
-
-    exitcode = yyparse(line);
-    if (exitcode != 0) {
-        return 1; // User asked to exit
-    }
-    if (line->error != 0) {
-        return 0; // Parse error, so skip this loop
-    }
-
-    /* printf("Command type: %c\n", cmnd_struct->type); */
-    /* printf("Command String: "); */
-    /* for (temp = cmnd_struct->first_token; temp != NULL; temp = temp->next) { */
-    /*     printf("%s ", temp->value); */
-    /* } */
-    /* printf("\n"); */
-
-
-    command *cmd = line->frst;
-    if (cmd == NULL) {
-        return 0;
-    }
-
+    /* // TODO: This might fail */
+    dir_home = getenv("HOME");  // Home directory of the user */
     char **tokens = tokenize(cmd);
+    token *temp;
 
-    // cd command. TODO: recognize chdir
+    
+    if (debug){
+        printf("Command String: ");
+        for (temp = cmd->first_token; temp != NULL; 
+             temp = temp->next) { 
+            printf("%s ", temp->value); 
+        }
+        printf("\n");
+        if (cmd->input_redirection != NULL){
+            printf("Input redirect to: ");
+            printf("%s ", cmd->input_redirection); 
+        } else if (cmd->output_redirection != NULL){
+            printf("Output redirect to: ");
+            printf("%s ", cmd->output_redirection); 
+        }
+    }
+    
+    // TODO: refactor to include all builtin commands
+    // cd command. TODO: recognize chdir, make this look nicer
     if ((strcmp("cd", cmd->first_token->value) == 0)) {
         if (cmd->first_token->next == NULL ||
                 !strcmp("~", cmd->first_token->next->value)){
@@ -115,29 +106,63 @@ int loop() {
 
         // TODO: check if pid = -1 (failed to fork)
         if (pid == 0) {
+            // child process
             // first argument is file to
             execvp(tokens[0], tokens);
-
             printf("That command could not be found.\n");
             exit(errno);
+        } else if (pid < 0){
+            // unsuccessful fork
+            exit(errno); // perhaps print?
         }
         else {
+            // parent process
             wait(NULL);
         }
     }
+}
 
-    // Freeing the memory
+
+void free_all(command *cmd, parsed* line){
     for (cmd = line->frst; cmd != NULL; cmd = cmd->next) {
         token *temp;
-
         for (temp = cmd->first_token; temp != NULL; temp = temp->next) {
             free(temp);
         }
-
         free(cmd);
     }
-
     free(line);
+}
+
+
+int loop() {
+    int exitcode = 0;
+
+    parsed *line = (parsed *) malloc(sizeof(parsed));
+    line->frst = NULL;
+    line->curr = NULL;
+    line->error = 0;
+    // TODO: null checks for malloc
+
+    print_prompt();
+    exitcode = yyparse(line);
+    // TODO: move logic here to a function which handles all builtins
+    if (exitcode != 0) {
+        return 1; // User asked to exit
+    }
+    if (line->error != 0) {
+        return 0; // Parse error, so skip this loop
+    }
+
+    
+    command *cmd = line->frst;
+    if (cmd == NULL) {
+        return 0;
+    }
+
+    
+    exec_cmd(cmd);
+    free_all(cmd, line);
 
     return exitcode;
 }
